88a5bcc847e6d2bae3d9f163c97b814a1a5b1423
rizkyrachman00 <rachman.ti2021@students.amikom.ac.id>
2025-04-19 23:18:57 +0700

    feat: example route POST for store expenses data

--- routes/expenses.ts
+++ routes/expenses.ts
@@ -1,11 +1,37 @@
 import { Hono } from "hono";
 
+type Expense = {
+  id: number;
+  title: string;
+  amount: number;
+};
+
+const fakeExpenses: Expense[] = [
+  {
+    id: 1,
+    title: "Groceries",
+    amount: 50,
+  },
+  {
+    id: 2,
+    title: "Utilities",
+    amount: 100,
+  },
+  {
+    id: 3,
+    title: "Rent",
+    amount: 1000,
+  },
+];
+
 export const expensesRoute = new Hono()
   .get("/", (c) => {
     return c.json({ expenses: [] });
   })
-  .post("/", (c) => {
-    return c.json({});
+  .post("/", async (c) => {
+    const expense = await c.req.json();
+    console.log({ expense });
+    return c.json(expense);
   });
-  // .delete
-  // .put
+// .delete
+// .put
