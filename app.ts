c2aaff977c7c979125c4f7bed7f0fa8b3475b628
rizkyrachman00 <rachman.ti2021@students.amikom.ac.id>
2025-04-19 22:53:10 +0700

    feat: init hono w/ test route

---
+++ app.ts
@@ -0,0 +1,8 @@
+import { Hono } from "hono";
+const app = new Hono();
+
+app.get('/test', c => {
+  return c.json({"message" : "test"})
+})
+
+export default app;
--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
+import app from "./app";
+
 Bun.serve({
-  fetch(req) {
-    return new Response("Hello from bun server");
-  },
+  fetch: app.fetch,
 });
 
 console.log("Server running");
